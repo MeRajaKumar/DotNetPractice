c6c654fef5a4dc0dcc4fde20f055a9d30936e0eb
Raja Kumar <105501989+MeRajaKumar@users.noreply.github.com>
2023-10-15 19:41:44 +0530

    New files uploaded

---
+++ factorialProgram/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num, fact As Integer
+        num = 0
+        fact = 1
+        System.Console.WriteLine("plz eneter any integer no:->")
+        num = Convert.ToInt32(Console.ReadLine())
+        While num > 0
+            fact = fact * num
+            System.Console.Write(num & " * ")
+            num = num - 1
+        End While
+        System.Console.WriteLine(Chr(8) & Chr(8) & " = " & fact)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ factorialProgramC#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 1;
+            Console.WriteLine("plz enter a number to print factorial :- ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if(num<0)
+            { Console.WriteLine("factorial of negative number " +num+ "doesn't exit, try again"); }
+            else
+            {
+                while (num > 0)
+                {
+                    i = i * num;
+                    Console.Write(num + " * ");
+                    num = num - 1;
+                }
+                    Console.Write("\b\b = {0}", i.ToString ());
+            }
+            Console.ReadKey();
+        }
+    }
+}
